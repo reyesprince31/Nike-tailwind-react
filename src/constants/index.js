b86e5ff6a648a1fa80994fb7c16ed3c3703cdbc0
Prince Reyes <77110899+reyesprince31@users.noreply.github.com>
2023-09-12 16:21:13 +0800

    change email

--- src/constants/index.js
+++ src/constants/index.js
@@ -133,7 +133,7 @@ export const footerLinks = [
   {
     title: "Get in touch",
     links: [
-      { name: "customer@nike.com", link: "mailto:customer@nike.com" },
+      { name: "reyes.prince31@gmail.com", link: "mailto:reyes.prince31@gmail.com" },
       { name: "+92554862354", link: "tel:+92554862354" },
     ],
   },
